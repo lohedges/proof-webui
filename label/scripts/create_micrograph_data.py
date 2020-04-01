1ae67e8ed41d3050a982075f9e1f31aebbd3a144
Lester Hedges <lester.hedges@gmail.com>
2020-04-01 09:49:33 +0100

    Added script for generating initial micrograph database records.

---
+++ label/scripts/create_micrograph_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Python script to generate initial micrograph records for the Django database.
+
+import glob
+import json
+import os
+
+# Figure out which directory we're in.
+if os.path.isdir("static/micrographs"):
+    base = ""
+elif os.path.isdir("label/static/micrographs"):
+    base = "label/"
+else:
+    raise Exception("Please run this script from within the top-level "
+                    "or 'label' directory.")
+
+# Glob all of the micrograph PNG images.
+micrographs = glob.glob(base + "static/micrographs/*png")
+
+# Create a list to store the JSON object.
+data = []
+
+# Loop over each micrograph and create a model entry.
+for pk, micrograph in enumerate(micrographs):
+    micrograph = micrograph.strip(base)
+    record = {
+               "model" : "label.Micrograph",
+               "pk" : pk,
+               "fields" : {
+                             "path" : micrograph
+                          }
+             }
+    data.append(record)
+
+# Dump the JSON data to a fixture file.
+with open(base + "fixtures/micrographs.json", "w") as f:
+    json.dump(data, f, indent=2)
