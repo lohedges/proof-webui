41c97308234dd647c927acf1d6cb4d71bfa23555
Lester Hedges <lester.hedges@gmail.com>
2020-04-01 09:43:01 +0100

    Added micrograph model.

--- label/models.py
+++ label/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django_mysql.models import ListTextField
 
-# Create your models here.
+class Micrograph(models.Model):
+    path = models.CharField(
+            max_length = 100,
+            help_text = "The path to the micrograph image."
+            )
+    ip_addresses = ListTextField(
+                base_field = models.CharField(max_length=39),
+                help_text = "A list of IP addresses that have uploaded labels "
+                           "for this micrograph."
+                )
+    num_labels = models.IntegerField(
+            default = 0,
+            help_text = "The number of uploaded labels for this micrograph."
+            )
